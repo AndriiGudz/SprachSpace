8d09cb3a4154fd903271b4959dfc859cab968495
Andrii Gudz <andrey.gudz@gmail.com>
2024-11-13 12:42:57 +0100

    Update Page Sing mob

---
+++ src/validationSchemas/SignUpSchema.ts
@@ -0,0 +1,23 @@
+import * as Yup from 'yup'
+import i18n from 'i18next'
+
+export const SignUpSchema = Yup.object().shape({
+  email: Yup.string()
+    .required(i18n.t('signinUp.emailRequired'))
+    .email(i18n.t('signinUp.invalidEmail'))
+    .matches(
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      i18n.t('signinUp.notAcceptableEmail')
+    ),
+  password: Yup.string()
+    .required(i18n.t('signinUp.passwordRequired'))
+    .min(8, i18n.t('signinUp.shortPassword'))
+    .matches(/[a-z]/, i18n.t('signinUp.smallLetter'))
+    .matches(/[A-Z]/, i18n.t('signinUp.capitalLetter'))
+    .matches(/\d/, i18n.t('signinUp.numberRequired'))
+    .matches(/[!@#$%^&*(),.?":{}|<>]/, i18n.t('signinUp.specialSymbol')),
+  termsAccepted: Yup.boolean().oneOf(
+    [true],
+    i18n.t('signinUp.termsAcceptance')
+  ),
+})
