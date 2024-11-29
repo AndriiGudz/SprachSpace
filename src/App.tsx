b8a331d926aa13cefc379ab81cb97164fcc44dd4
Andrii Gudz <andrey.gudz@gmail.com>
2024-11-29 19:38:43 +0100

    bag fix i18n & validationSchemas

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,73 @@
-import { Suspense, useEffect } from 'react'
-import { useTranslation } from 'react-i18next'
-import './i18n'
-import GlobalStyles from './styles/GlobalStyles'
-import CssBaseline from '@mui/material/CssBaseline'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layout from './components/Layout/Layout'
-import Home from './pages/Home/Home'
-import PageSignInUp from './pages/PageSignInUp/PageSignInUp'
-import Profile from './pages/Profile/Profile'
-import ProtectedRoute from '../src/components/ProtectedRoute/ProtectedRoute'
-import theme from './theme'
-import { ThemeProvider } from '@mui/material/styles'
-import { Provider, useDispatch } from 'react-redux'
-import { store } from './store/store'
-import Notifications from './components/Notifications/Notifications'
-import { setTokens, setUser } from './store/redux/userSlice/userSlice'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import './i18n';
+import GlobalStyles from './styles/GlobalStyles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
+import PageSignInUp from './pages/PageSignInUp/PageSignInUp';
+import Profile from './pages/Profile/Profile';
+import ProtectedRoute from '../src/components/ProtectedRoute/ProtectedRoute';
+import theme from './theme';
+import { ThemeProvider } from '@mui/material/styles';
+import Notifications from './components/Notifications/Notifications';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useDispatch } from 'react-redux';
+import { setTokens, setUser } from './store/redux/userSlice/userSlice';
 
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const { t, i18n } = useTranslation();
 
+  // Загрузка данных пользователя из localStorage при инициализации приложения
   useEffect(() => {
-    const storedUser = localStorage.getItem('user')
+    const storedUser = localStorage.getItem('user');
     if (storedUser) {
-      const userData = JSON.parse(storedUser)
-      dispatch(setUser(userData))
+      const userData = JSON.parse(storedUser);
+      dispatch(setUser(userData));
       dispatch(
         setTokens({
           accessToken: userData.accessToken,
           refreshToken: userData.refreshToken,
         })
-      )
+      );
     }
-  }, [dispatch])
+  }, [dispatch]);
 
-  const { t, i18n } = useTranslation()
-
+  // Обновление title страницы при изменении языка
   useEffect(() => {
-    // Логика с title и описанием
-  }, [t, i18n])
+    if (i18n.isInitialized) {
+      document.title = t('app.title');
+    }
+  }, [i18n.language]); // Используем только i18n.language, чтобы обработать изменения языка
 
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <CssBaseline />
+          <GlobalStyles />
           <Notifications />
-          <ThemeProvider theme={theme}>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <CssBaseline />
-              <GlobalStyles />
-              <Routes>
-                <Route path="/signin" element={<PageSignInUp />} />
-                <Route path="/" element={<Layout />}>
-                  <Route index element={<Home />} />
-                  {/* Защищенный маршрут для /profile и др.*/}
-                  <Route
-                    path="/profile"
-                    element={
-                      <ProtectedRoute>
-                        <Profile />
-                      </ProtectedRoute>
-                    }
-                  />
-                </Route>
-              </Routes>
-            </LocalizationProvider>
-          </ThemeProvider>
-        </Suspense>
-      </BrowserRouter>
-    </Provider>
-  )
+          <Routes>
+            <Route path="/signin" element={<PageSignInUp />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+          </Routes>
+        </LocalizationProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
